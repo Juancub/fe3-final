0b24239a8957edbdd55f65b4089ef2615fa7a04b
juancub <juanuribe.ber@gmail.com>
2023-04-05 20:47:28 -0500

    terminando los componentes y las rutas

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState} from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- 
+
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(response => setData(response.data))
+      .catch(error => console.log(error));
+  }, [id]);
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
   return (
     <>
       <h1>Detail Dentist id </h1>
+      {console.log(data)}
+      <h3>{data.name}</h3>
+      <p>{data.email}</p>
+      <p>{data.phone}</p>
+      <p>{data.website}</p>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
     </>
